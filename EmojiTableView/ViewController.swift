01d4193cdfc43930b7eff9cc5ea20b6d5d7b1e68
Manuel Kremkow <manuel.kremkow@gmx.de>
2017-03-23 11:11:47 +0100

    Initial Commit

---
+++ EmojiTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiTableView
+//
+//  Created by Manuel Kremkow on 23.03.17.
+//  Copyright © 2017 Manuel Kremkow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
